7822aaaa6c4c8f0ed40c3384c5e5a55558d91bbb
Victor Sanchez <vsanantone@gmail.com>
2023-04-12 15:20:54 -0500

    Add the comparison of variables using the comparison and logical operators with commented text

--- comparisons.rb
+++ comparisons.rb
@@ -17,29 +17,41 @@ number_students = 20
 string_teachers = "4"
 numberDogs = 0
 
-# EXAMPLE: print the result of the comparison: is number_seachers greater than number_students?
+# EXAMPLE: print the result of the comparison: is number_teachers greater than number_students?
 puts "Is number_teachers greater than number_students?", number_teachers > number_students
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?",
+number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?",
+number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 20?",
+number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_students less than or equal to 20?",
+number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?",
+number_students <= 21
 # this should print: true
 
 
@@ -53,21 +65,21 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: This comparison is asking "is 4 less than 9?" The answer is yes, so we will see true in the console.
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+#YOU DO: This comparison is asking "is 4 less than the variable books?" The answer is no since books has been the assigned tha value of 3 (4 < 3), so we will see false in the console.
 
 friends = 6
 siblings = 2
 puts friends > siblings
-# YOU DO: Explain.
+# YOU DO: This comparison is asking "is the variable friends with an assigned value of 6 greater than the variable siblings with an assigned value of 2 (6 > 2). The answer is yes, so we will see true in the console.
 
 attendees = 9
 meals = 8
 puts attendees != meals
-# YOU DO: Explain.
+# YOU DO: This comparison is asking "is the variable attendees with an assigned value of 9 not equal to the variable meals with an assigned value of 8 (9 !== 8). The answer is yes, so we will see true in the console.
 
 
 #-------------------
@@ -79,9 +91,9 @@ finished_homework = false
 
 # EXAMPLE:
 # Determine if the user is hungry and has completed their homework
-puts is_hungry && finished_homework
+# puts is_hungry && finished_homework
 # Determine if the user is hungry or has completed their homework
-puts is_hungry || finished_homework
+# puts is_hungry || finished_homework
 
 loves_to_play = true
 loves_dog_park = false
@@ -90,15 +102,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
+puts loves_to_play && loves_dog_park
 
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age <= 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: My final line of code evaluated to true, because most dogs are mentally still a puppy at 1 yr old. Some breeds may reach physical maturity by 1 yr, but that doesn't make them an adult simlarly like most awkward 6ft teenagers. I would suggest dogs are adults between the 18-36 month period exceeding the 12 month age of our current puppy.
