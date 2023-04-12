ae9a6bfd010e71141f97180c93b875e4f99c955c
Victor Sanchez <vsanantone@gmail.com>
2023-04-12 16:23:38 -0500

    Add the mild challenge if else statement for hw assignment

---
+++ code_challenges/mild.rb
@@ -0,0 +1,12 @@
+## Mild
+
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+
+integer = 2
+
+if integer.even?
+    puts "even"
+else
+puts "odd"
+end
